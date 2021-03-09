3678cf522f0002e358915eb75315aada9d7450b2
SojuAdmin <>
2021-03-09 14:37:51 +0700

    update

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -81,7 +81,7 @@ const config: MenuEntry[] = [
       },
       {
         label: 'Blog',
-        href: 'https://medium.com',
+        href: 'https://medium.com/@SojuFinance',
       },
     ],
   },
